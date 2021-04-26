430fe0464f22c1953e6ce7671b5cdcbce67d36c8
tuanthanhnguyen <thanhbeo5n@gmail.com>
2021-04-26 10:12:39 +0700

    Hashrate & result sending changes

--- main.py
+++ main.py
@@ -1,62 +1,120 @@
 #!/usr/bin/env python3
 # Minimal version of Duino-Coin PC Miner, useful for developing own apps. Created by revox 2020-2021
-import socket, hashlib, urllib.request, time, os, sys  # Only python3 included libraries
+import hashlib
+import os
+import socket
+import sys  # Only python3 included libraries
+import time
+import urllib.request
 
 soc = socket.socket()
 soc.settimeout(10)
 
 username = "thanhbeo12345"  # Edit this to your username, mind the quotes
-UseLowerDiff = True # Set it to True to mine with lower difficulty
+UseLowerDiff = False  # Set it to True to mine with lower difficulty
 
+
+def retrieve_server_ip():
+    print("> Retrieving Pool Address And Port")
+    pool_obtained = False
+    while not pool_obtained:
+        try:
+            serverip = ("https://raw.githubusercontent.com/"
+                            + "revoxhere/"
+                            + "duino-coin/gh-pages/"
+                            + "serverip.txt")
+            with urllib.request.urlopen(serverip) as content:
+                # Read content and split into lines
+                content = content.read().decode().splitlines()
+            global pool_address, pool_port
+            # Line 1 = IP
+            pool_address = content[0]
+            # Line 2 = port
+            pool_port = content[1]
+            pool_obtained =  True
+        except:
+            print("> Failed to retrieve Pool Address and Port, Retrying.")
+            continue
+
+retrieve_server_ip()
 while True:
     try:
-        # This sections grabs pool adress and port from Duino-Coin GitHub file
-        serverip = "https://raw.githubusercontent.com/revoxhere/duino-coin/gh-pages/serverip.txt"  # Serverip file
-        with urllib.request.urlopen(serverip) as content:
-            content = (
-                content.read().decode().splitlines()
-            )  # Read content and split into lines
-        pool_address = content[0]  # Line 1 = pool address
-        pool_port = content[1]  # Line 2 = pool port
-
         # This section connects and logs user to the server
-        soc.connect((str(pool_address), int(pool_port)))  # Connect to the server
+        soc.connect((str(pool_address), int(pool_port)))
         server_version = soc.recv(3).decode()  # Get server version
         print("Server is on version", server_version)
+
         # Mining section
         while True:
             if UseLowerDiff:
-                soc.send(
-                    bytes("JOB," + str(username) + ",MEDIUM", encoding="utf8")
-                )  # Send job request
+                # Send job request for lower diff
+                soc.send(bytes(
+                    "JOB,"
+                    + str(username)
+                    + ",MEDIUM",
+                    encoding="utf8"))
             else:
-                soc.send(
-                    bytes("JOB," + str(username), encoding="utf8")
-                )  # Send job request
-            job = soc.recv(1024).decode()  # Get work from pool
-            job = job.split(",")  # Split received data to job (job and difficulty)
-            difficulty = job[2]
+                # Send job request
+                soc.send(bytes(
+                    "JOB,"
+                    + str(username),
+                    encoding="utf8"))
 
-            for result in range(
-                100 * int(difficulty) + 1
-            ):  # Calculate hash with difficulty
-                ducos1 = hashlib.sha1(
-                    str(job[0] + str(result)).encode("utf-8")
-                ).hexdigest()  # Generate hash
-                if job[1] == ducos1:  # If result is even with job
-                    soc.send(
-                        bytes(str(result) + ",,Minimal_PC_Miner", encoding="utf8")
-                    )  # Send result of hashing algorithm to pool
-                    feedback = soc.recv(1024).decode()  # Get feedback about the result
-                    if feedback == "GOOD":  # If result was good
-                        print("Accepted share", result, "Difficulty", difficulty)
-                        time.sleep(1)
+            # Receive work
+            job = soc.recv(1024).decode().rstrip("\n")
+            # Split received data to job and difficulty
+            job = job.split(",")
+            difficulty = job[2]
+            
+            hashingStartTime = time.time()
+            base_hash = hashlib.sha1(str(job[0]).encode('ascii'))
+            temp_hash = None
+            
+            for result in range(100 * int(difficulty) + 1):
+                # Calculate hash with difficulty
+                temp_hash =  base_hash.copy()
+                temp_hash.update(str(result).encode('ascii'))
+                ducos1 = temp_hash.hexdigest()
+
+                # If hash is even with expected hash result
+                if job[1] == ducos1:
+                    hashingStopTime = time.time()
+                    timeDifference = hashingStopTime - hashingStartTime
+                    hashrate = result / timeDifference
+
+                    # Send numeric result to the server
+                    soc.send(bytes(
+                        str(result)
+                        + ","
+                        + str(hashrate)
+                        + ",Minimal_PC_Miner",
+                        encoding="utf8"))
+
+                    # Get feedback about the result
+                    feedback = soc.recv(1024).decode().rstrip("\n")
+                    # If result was good
+                    if feedback == "GOOD":
+                        print("Accepted share",
+                              result,
+                              "Hashrate",
+                              int(hashrate/1000),
+                              "kH/s",
+                              "Difficulty",
+                              difficulty)
                         break
-                    elif feedback == "BAD":  # If result was bad
-                        print("Rejected share", result, "Difficulty", difficulty)
+                    # If result was incorrect
+                    elif feedback == "BAD":
+                        print("Rejected share",
+                              result,
+                              "Hashrate",
+                              int(hashrate/1000),
+                              "kH/s",
+                              "Difficulty",
+                              difficulty)
                         break
+
     except Exception as e:
         print("Error occured: " + str(e) + ", restarting in 5s.")
+        retrieve_server_ip()
         time.sleep(5)
-        print("shutting down...")
-        exit()
+        os.execl(sys.executable, sys.executable, *sys.argv)
